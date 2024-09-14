8a2fcd0cdbd707bab2e00a762804bd4864d9cac3
duedull <mr.duedull@gmail.com>
2024-09-15 00:48:16 +0530

    seconf commit, tested some global variables in node

---
+++ 1-app.js
@@ -0,0 +1,18 @@
+const no = 12;
+
+if (no > 10) {
+    console.log('large number');
+} else {
+    console.log('small number');
+}
+
+console.log(`hey this is my first node app`);
+
+console.log('hey nigga');
+
+// some important global variables:
+//__dirname
+//__filename
+//require - function to use modules
+//module - info about current modules
+//process - info about the enviorment where the program is being executed
