bbef6e03931386a1e3aa3d98c7963fc6696844c6
duedull <mr.duedull@gmail.com>
2024-09-15 18:57:46 +0530

    worked on fs-File System synchronous blocking part
    writeFileSync() and readFileSync()

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-const path = require('path');
-
-console.log(path.sep);
+//fs 
 
 
-const filePath = path.join('./f1', 'f2', 'file1.txt');
+const { readFileSync, writeFileSync, } = require('fs');
 
-console.log(filePath);
 
-const absolute_path = path.resolve(__dirname, 'f1', 'f2', 'file1.txt');
+//to read an already existing file
+const first = readFileSync('./f1/first.txt', 'utf8');
+const second = readFileSync('./f1/second.txt', 'utf8');
+console.log(first + ' ' + second);
 
-console.log(absolute_path);
+
+//to write or create a new file with some data in it
+// writeFileSync('./f1/f2/generated-file', `hey this is the generated file and the contents are ${first} and ${second}`);
+
+
+// this {flag: 'a'} at the end is used to append the contents of the existing file
+writeFileSync('./f1/f2/generated-file', `hey this is the generated file and the contents are ${first} and ${second}`, { flag: 'a' });
