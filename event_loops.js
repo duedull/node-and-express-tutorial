a120ade6778c6a8e21504d23e5cb3cc621ac3a73
duedull <mr.duedull@gmail.com>
2024-09-16 21:56:02 +0530

    created a file event_loop.js

---
+++ event_loops.js
@@ -0,0 +1,11 @@
+// starting the operating system process
+
+console.log('first');
+
+setTimeout(() => {
+    console.log('second');
+}, 0);
+
+console.log('third');
+
+//compleated the operating system process
