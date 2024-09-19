d8286dbd656b39785d25a1543d3048549b116ec9
duedull <mr.duedull@gmail.com>
2024-09-19 15:22:46 +0530

    done with the async patterns

---
+++ fs-using-async-await.js
@@ -0,0 +1,20 @@
+const { readFile, writeFile } = require('fs').promises;
+
+let first = './f1/first.txt';
+let second = './f1/second.txt';
+
+const funAppend = async () => {
+    try {
+        const first = await readFile('./f1/first.txt', 'utf8')
+        const second = await readFile('./f1/second.txt', 'utf8')
+        console.log(first, second);
+        await writeFile('./f1/new-file.txt', `this is the new file made with the contents of first.txt and second.txt: ${first}  ${second}`, { flag: 'a' });
+
+        // console.log(third);
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+funAppend();
--- readFile-promise.js
+++ readFile-promise.js
@@ -1,18 +1,33 @@
 const { readFile } = require('fs');
 
-function getting_promise(path) {
-    return new Promise((resolve, reject) => {
-        readFile(path, 'utf-8', (err, result) => {
-            if (err) {
-                reject(err);
-            } else {
+// function getting_promise(path) {
+//     return new Promise((resolve, reject) => {
+//         readFile(path, 'utf-8', (err, result) => {
+//             if (err) {
+//                 reject(err);
+//             } else {
 
-                resolve(result);
-            }
-        })
-    })
+//                 resolve(result);
+//             }
+//         })
+//     })
+// }
+
+// getting_promise('./f1/first.txt')
+//     .then(result => console.log(result))
+//     .catch(err => console.log(err));
+
+
+
+//DOING THE SAME WITH ASYNC-AWAIT
+
+async function getting_promise(path) {
+    try {
+        const first = await getText(path);
+        console.log(first);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
-getting_promise('./f1/first.txt')
-    .then(result => console.log(result))
-    .catch(err => console.log(err));
+getting_promise('../f1/first.txt');
